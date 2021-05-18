4e71eb9eaa1c1ed9fcb48e3871c711ea050a15b9
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-18 14:22:11 +0530

    Add files via upload

---
+++ Comparison Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Comparison_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int firstNumber = 10, secondNumber = 20;
+			bool result;
+
+			result = (firstNumber == secondNumber);
+			Console.WriteLine("{0} == {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber > secondNumber);
+			Console.WriteLine("{0} > {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber < secondNumber);
+			Console.WriteLine("{0} < {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber >= secondNumber);
+			Console.WriteLine("{0} >= {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber <= secondNumber);
+			Console.WriteLine("{0} <= {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber != secondNumber);
+			Console.WriteLine("{0} != {1} returns {2}", firstNumber, secondNumber, result);
+
+		}
+	}
+}
