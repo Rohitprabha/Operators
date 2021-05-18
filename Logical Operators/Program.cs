bdacc2855b592d16bcf33cc3a43bda2dd7cae07f
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-18 14:25:40 +0530

    Add files via upload

---
+++ Logical Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Logical_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 10, secondNumber = 20;
+            bool result;
+            result = (firstNumber == secondNumber) || (firstNumber > 5);
+            Console.WriteLine(result);
+            result = (firstNumber == secondNumber) && (firstNumber > 5);
+            Console.WriteLine(result);
+        }
+    }
+}
