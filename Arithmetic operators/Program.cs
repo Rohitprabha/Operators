564b3312ba11d2968547816662118b1716df9d90
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-18 14:19:20 +0530

    Add files via upload

---
+++ Arithmetic operators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Arithmetic_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5, num2 = 2, result;
+            result = num1 + num2;
+            Console.WriteLine("Value is {0}", result);
+            result = num1 - num2;
+            Console.WriteLine("Value is {0}", result);
+            result = num1 * num2;
+            Console.WriteLine("Value is {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("Value is {0}", result);
+            result = num1 % num2;
+            Console.WriteLine("Value is {0}", result);
+            result = num1++;
+            Console.WriteLine("Value is {0}", result);
+            result = ++num1;
+            Console.WriteLine("Value is {0}", result);
+            result = num1--;
+            Console.WriteLine("Value is {0}", result);
+            result = --num1;
+            Console.WriteLine("Value is {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
