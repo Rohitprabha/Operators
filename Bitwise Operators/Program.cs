fa9dc391a2d180619a6c53520b3b1878c928f73d
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-18 14:27:47 +0530

    Add files via upload

---
+++ Bitwise Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Bitwise_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 7;
+			int secondNumber = 4;
+			int result;
+
+			result = ~firstNumber;
+			Console.WriteLine("~{0} = {1}", firstNumber, result);
+
+			result = firstNumber & secondNumber;
+			Console.WriteLine("{0} & {1} = {2}", firstNumber,secondNumber, result);
+
+			result = firstNumber | secondNumber;
+			Console.WriteLine("{0} | {1} = {2}", firstNumber,secondNumber, result);
+
+			result = firstNumber ^ secondNumber;
+			Console.WriteLine("{0} ^ {1} = {2}", firstNumber,secondNumber, result);
+
+			result = firstNumber << 1;
+			Console.WriteLine("{0} << 1 = {1}", firstNumber, result);
+
+			result = firstNumber >> 1;
+			Console.WriteLine("{0} >> 1 = {1}", firstNumber, result);
+        }
+    }
+}
